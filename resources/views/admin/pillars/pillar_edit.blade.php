1fbb4ae5c3759cb57104c44bb97fb06dfcac8afd
Yaser Bahrami <yaser.bahrami@gmail.com>
2023-11-01 10:47:04 +0330

    ix bug

--- resources/views/admin/pillars/pillar_edit.blade.php
+++ resources/views/admin/pillars/pillar_edit.blade.php
@@ -6,7 +6,6 @@
             <div class="card-body">
                 <div class="nav nav-tabs nav-fill" id="nav-tab" role="tablist">
                     <button class="nav-link active" id="nav-farsi-tab" data-toggle="tab" data-target="#nav-farsi" type="button" role="tab" aria-controls="nav-farsi" aria-selected="true">بخش فارسی</button>
-                    <button class="nav-link" id="nav-english-tab" data-toggle="tab" data-target="#nav-english" type="button" role="tab" aria-controls="nav-english" aria-selected="false">بخش انگلیسی </button>
                 </div>
                 <form method="post" action="/admin/pillar/{{$pillar->id}}" enctype="multipart/form-data">
                     {{csrf_field()}}
@@ -60,41 +59,7 @@
 
                             </div>
                         </div>
-                        <div class="tab-pane fade" id="nav-english" role="tabpanel" aria-labelledby="nav-english-tab">
-                            <!-- English -->
-                            <div class="tab-pane fade show active" id="nav-farsi" role="tabpanel" aria-labelledby="nav-farsi-tab">
-                                <div class="alert alert-warning mt-1 mb-5">
-                                    پر کردن فیلدهای ستاره دار اجباریست
-                                </div>
-                                <div class="col-12 col-md-6">
 
-                                    <div class="input-group mb-3">
-                                        <div class="input-group-prepend">
-                                            <span class="input-group-text" >نام انگلیسی<span class="text-danger">*</span></span>
-                                        </div>
-                                        <input type="text" class="form-control"   id="fname_en" name="fname_en" value="{{old('fname_en',$pillar->fname_en)}}" />
-                                    </div>
-                                    <div class="input-group mb-3">
-                                        <div class="input-group-prepend">
-                                            <span class="input-group-text">نام خانوادگی انگلیسی<span class="text-danger">*</span></span>
-                                        </div>
-                                        <input type="text" class="form-control"  id="lname_en" name="lname_en" value="{{old('lname_en',$pillar->lname_en)}}" />
-                                    </div>
-                                    <div class="input-group mb-3">
-                                        <div class="input-group-prepend">
-                                            <span class="input-group-text" > سمت انگلیسی<span class="text-danger">*</span></span>
-                                        </div>
-                                        <input type="text" class="form-control"  id="position_fa" name="position_en" value="{{old('position_en',$pillar->position_en)}}" />
-                                    </div>
-
-                                    <div class="input-group mb-3">
-                                        <label for="biography_en">بیوگرافی انگلیسی:<span class="text-danger">*</span></label>
-                                        <textarea id="biography_fa" name="biography_en">{{old('biography_en',$pillar->biography_en)}}</textarea>
-                                    </div>
-
-                                </div>
-                            </div>
-                        </div>
                     </div>
                     <button class="btn btn-success">بروزرسانی ارکان</button>
                 </form>
