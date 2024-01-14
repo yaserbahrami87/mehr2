838dca37f433c85bdf140ea376af597e71c3d381
Yaser Bahrami <yaser.bahrami@gmail.com>
2024-01-14 11:28:29 +0330

    competition

--- resources/views/admin/competition/competition_all.blade.php
+++ resources/views/admin/competition/competition_all.blade.php
@@ -7,12 +7,18 @@
             <div class="row">
                 @foreach($competitions as $competition)
                     <div class="col-12 col-md-3">
+
                         <div class="card" >
                             <img src="/images/competition/thumbnail_{{$competition->image}}" class="card-img-top" alt="..." height="160px">
                             <div class="card-body">
                                 <p > نام اثر: <b class="card-title">{{$competition->title}}</b></p>
 
                                 <p>توضیحات: <b class="card-text">{{$competition->description}}</b></p>
+                            </div>
+                            <div class="card-footer">
+                                <a href="/images/competition/{{$competition->image}}" class="btn btn-primary btn-sm   " target="_blank">
+                                    مشاهده
+                                </a>
                             </div>
                         </div>
 
